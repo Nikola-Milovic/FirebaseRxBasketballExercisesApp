45ca2e1fcfdb0babb5b2c338fd8b0efcd4d6c6d4
Nikola Milovic <nikolamilovic2001@gmail.com>
2020-02-29 13:30:20 +0100

    repository working properly

---
+++ app/src/test/java/com/nikolam/basketpro/data/remote/FakeRemoteDataSource.kt
@@ -0,0 +1,19 @@
+package com.nikolam.basketpro.data.remote
+
+import com.nikolam.basketpro.data.DataSource
+import com.nikolam.basketpro.model.Drill
+import com.nikolam.basketpro.model.DrillDetail
+import com.nikolam.basketpro.model.DrillsType
+import io.reactivex.Observable
+
+class FakeRemoteDataSource () : DataSource {
+        override fun loadDrillTypes(): Observable<DrillsType> = Observable.just(DrillsType("XXX", "XXX"))
+
+        override fun loadDrillList(drillType: String): Observable<Drill> {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun loadDrillDetails(id: String): io.reactivex.rxjava3.core.Single<DrillDetail> {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+}
